2d5c8f07ea17c8b7c4ff0503659f2683b3572f59
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-09-12 03:39:34 -0300

    TesteV1

--- dev/src/main/java/com/deuslley/dev/MathController.java
+++ dev/src/main/java/com/deuslley/dev/MathController.java
@@ -29,7 +29,7 @@ public class MathController {
     private boolean isNumeric(String strNumber) {
         if(strNumber == null) return false;
         String number = strNumber.replaceAll("," , ".");
-        return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 
 }
